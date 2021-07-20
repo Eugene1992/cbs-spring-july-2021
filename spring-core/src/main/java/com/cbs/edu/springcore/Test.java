2b07a6d975eedd24e93d14ebc5692860bed88fa7
Yevhenii Deineka <yevhenii.deineka@netcracker.com>
2021-07-20 16:07:23 +0300

    test notifications

--- spring-core/src/main/java/com/cbs/edu/springcore/Test.java
+++ spring-core/src/main/java/com/cbs/edu/springcore/Test.java
@@ -3,5 +3,7 @@ package com.cbs.edu.springcore;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
     }
 }
